fa9de2b32697a5f86609bf0bc96136c6089ba9a0
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2026-06-12 21:36:59 +0200

    Added function updateRadioUI

--- js/app.js
+++ js/app.js
@@ -2,6 +2,28 @@ const radioContainer = document.querySelector(".radio-input-container");
 const metricRadio = document.getElementById("radio-metric");
 const imperialRadio = document.getElementById("radio-imperial");
 
+
+
+function updateRadioUI(radio) {
+  const radioBtns = document.querySelectorAll(".radio-input");
+  const radioClicked = radio;
+  // Reset styles from radio inputs
+  radioBtns.forEach((radio) => {
+    radio.removeAttribute("checked");
+    radio.classList.remove("radio-input-checked");
+  })
+
+  // Add styles to clicked radio input
+  radioClicked.setAttribute("checked", "");
+  radioClicked.classList.add("radio-input-checked");
+  
+  updateBodyInputs(radioClicked.id)
+}
+
+function updateBodyInputs(unit) {
+  console.log(unit)
+}
+
 radioContainer.addEventListener("change", (e) => {
-  console.log(e.target)
+  updateRadioUI(e.target);
 })
