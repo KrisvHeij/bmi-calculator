c2c9e6a9420505bef9a1b872b05f0497432de417
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2026-06-12 20:53:51 +0200

    Updated

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const radioContainer = document.querySelector(".radio-input-container");
+const metricRadio = document.getElementById("radio-metric");
+const imperialRadio = document.getElementById("radio-imperial");
+
+radioContainer.addEventListener("change", (e) => {
+  
+})
